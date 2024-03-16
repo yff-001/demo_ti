7562f36acc0027351d7b1838edc57ec3ba144d58
Yufeng Xu <yufeng.xu@live.com>
2024-03-16 17:49:16 +0800

    first commit

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LDS_SRCS += \
+../mspm0l1306.lds 
+
+C_SRCS += \
+../empty_non_sysconfig.c \
+../startup_mspm0l130x_gcc.c \
+../ti_msp_dl_config.c 
+
+C_DEPS += \
+./empty_non_sysconfig.d \
+./startup_mspm0l130x_gcc.d \
+./ti_msp_dl_config.d 
+
+OBJS += \
+./empty_non_sysconfig.o \
+./startup_mspm0l130x_gcc.o \
+./ti_msp_dl_config.o 
+
+OBJS__QUOTED += \
+"empty_non_sysconfig.o" \
+"startup_mspm0l130x_gcc.o" \
+"ti_msp_dl_config.o" 
+
+C_DEPS__QUOTED += \
+"empty_non_sysconfig.d" \
+"startup_mspm0l130x_gcc.d" \
+"ti_msp_dl_config.d" 
+
+C_SRCS__QUOTED += \
+"../empty_non_sysconfig.c" \
+"../startup_mspm0l130x_gcc.c" \
+"../ti_msp_dl_config.c" 
+
+
